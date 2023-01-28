58bfdb0267ae5539fa4050158f083fe3a62d7c34
Uthaeus <romanlavery@gmail.com>
2023-01-28 16:55:29 -0700

    created new meetup form

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,8 +1,12 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 
 const NewMeetupPage = () => {
     return (
-        <div>New Meetup Page</div>
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm />
+        </section>
     );
 };
 
