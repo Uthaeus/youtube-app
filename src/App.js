66db76be47721bf05d8cab6335eef8f8f8287f41
Uthaeus <romanlavery@gmail.com>
2023-01-28 13:55:09 -0700

    restructured for lesson

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from "react";
 
-import Todo from "./components/Todo";
-
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text='Learn React' />
-      <Todo text='Master React' />
-      <Todo text='something something something' />
     </div>
   );
 }
