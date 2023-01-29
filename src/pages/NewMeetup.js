98e5714d3ad2b1a6ba3c5b31806d4e462572a843
Uthaeus <romanlavery@gmail.com>
2023-01-28 17:15:50 -0700

    created new meetup form and handler

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,10 +2,14 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 
 const NewMeetupPage = () => {
+    const addMeetupHandler = (meetupData) => {
+        
+    }
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </section>
     );
 };
